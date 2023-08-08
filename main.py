d37bb93e080806c9f506c394ee8c9c6c78af09f9
S10258105 <S10258105@connect.np.edu.sg>
2023-08-08 17:05:16 +0800

    main

---
+++ main.py
@@ -0,0 +1,25 @@
+# imports the three modules named cash_on_hand, profit_loss, and overheads
+import cash_on_hand, profit_loss, overheads
+
+def main():
+    """
+    - Function executes all 3 functions in the three modules
+    - No parameters required
+
+    """
+
+    # calls the overhead_function() from the overheads module
+    # Execute the function to calculate and write the highest overhead to a summary report.
+    overheads.overhead_function()
+
+    # calls the coh_function() from the cash_on_hand module
+    # Execute the function to calculate and write the cash deficits or highest surplus to summary report
+    cash_on_hand.coh_function()
+
+    # calls the profitloss_function() from the profit_loss module
+    # Execute the function to calculate and write the net profit deficits or highest surplus to summary report
+    profit_loss.profitloss_function()
+
+# initiates the execution of the tasks specified in the overhead_function, 
+# coh_function, and profitloss_function
+main()
